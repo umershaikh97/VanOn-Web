ad35a02542733f09135b8483044998e2a0caa92e
Umer Shaikh <umershaikh305@gmail.com>
2019-11-23 23:55:39 +0500

    Install redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
+import { BrowserRouter } from 'react-router-dom';
 import withRoot from './withRoot';
 
 const App = (props) => {
   return (
-    <>
+    <BrowserRouter>
       <AppRoutes />
-    </>
+    </BrowserRouter>
   );
 }
 
