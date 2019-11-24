12855566c666995129169dd0ee588486985d45d8
Umer Shaikh <umershaikh305@gmail.com>
2019-11-25 00:00:49 +0500

    Update AuthReducer

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,5 +1,5 @@
 const initialState = {
-    isAuthenticated: false,
+    isAuthenticated: true,
     role: '',
     username: '',
     password: '',
@@ -10,10 +10,10 @@ const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'LOGIN':
             if (action.payload.username === 'admin' && action.payload.password === '123456') {
-                return { ...state, role: 'admin', isAuthenticated: true }
+                return { ...state, role: 'admin', isAuthenticated: true, username: 'admin', password: '12345' }
             }
             else if (action.payload.username === 'vendor' && action.payload.password === '123456') {
-                return { ...state, role: 'vendor', isAuthenticated: true }
+                return { ...state, role: 'vendor', isAuthenticated: true, username: 'vendor', password: '12345' }
             }
             else {
                 return { ...state, loginError: 'Invalid Username or Password' }
