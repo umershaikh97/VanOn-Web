0b389f449266a4390c711e1a126d25dd154fd36a
Umer Shaikh <umershaikh305@gmail.com>
2019-11-24 19:07:23 +0500

    Update store implementation

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './store/reducers/rootReducer';
-import thunk from 'redux-thunk';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
+import store from './store';
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
