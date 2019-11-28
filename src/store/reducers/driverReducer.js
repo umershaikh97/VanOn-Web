8e68866e36b8ed11fda64745327c4e2978a749c7
Umer Shaikh <umershaikh305@gmail.com>
2019-11-28 23:19:04 +0500

    Update Driver data

--- src/store/reducers/driverReducer.js
+++ src/store/reducers/driverReducer.js
@@ -1,7 +1,7 @@
 const initialState = {
     data: [
-        { name: 'Driver1', email: 'driver1@vanon.com', phone: 123, cnic: 614144443, license: 614144443, van: 'BBW-2345', area: 'Gulshan', v_id: '1' },
-        { name: 'Driver2', email: 'driver2@vanon.com', phone: 123, cnic: 614144443, license: 614144443, van: 'BBW-2345', area: 'Johar', v_id: '2' },
+        { name: 'Driver1', email: 'driver1@vanon.com', phone: '0348-9097792', cnic: '42101-452250053-5', license: 614144443, van: 'BBW-2345', area: 'Gulshan', v_id: '1' },
+        { name: 'Driver2', email: 'driver2@vanon.com', phone: '0348-9097792', cnic: '42101-452250053-5', license: 614144443, van: 'BBW-2345', area: 'Johar', v_id: '2' },
     ]
 }
 
