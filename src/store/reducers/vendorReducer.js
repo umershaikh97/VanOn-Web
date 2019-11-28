13eb9c1959934a630567b846e2b10cdd5c55f173
Umer Shaikh <umershaikh305@gmail.com>
2019-11-28 23:18:45 +0500

    Update Vendor data

--- src/store/reducers/vendorReducer.js
+++ src/store/reducers/vendorReducer.js
@@ -1,7 +1,7 @@
 const initialState = {
     data: [
-        { name: 'Vendor1', email: 'vendor1@vanon.com', phone: 123, cnic: 614144443 },
-        { name: 'Vendor2', email: 'vendor2@vanon.com', phone: 123, cnic: 614144443 },
+        { name: 'Vendor1', email: 'vendor1@vanon.com', phone: '0348-9097792', cnic: '42101-452250053-5' },
+        { name: 'Vendor2', email: 'vendor2@vanon.com', phone: '0321-2570252', cnic: '42101-42225553-5' },
     ]
 }
 
