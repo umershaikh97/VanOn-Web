01ecec506ce817597a2b8a9b8be13fba98b6a148
Umer Shaikh <umershaikh305@gmail.com>
2020-01-08 01:24:21 +0500

    Implement JWT for Authentication

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,3 +1,7 @@
+import Axios from "axios";
+import { API } from "../../API";
+import { checkArrayLength, checkKeyInObject } from "../../utils/utils";
+
 // Without thunk we use it like this, but as we are using thunk middleware, we can return a function
 // export const createProject = (project) => {
 //     return { type: 'CREATE_PROJECT', payload: project };
@@ -44,8 +48,68 @@
 // };
 
 export const login = (loginDetails) => {
-    return (dispatch, getState) => {
-        dispatch({ type: 'LOGIN', payload: loginDetails })
+    return async (dispatch, getState) => {
+        dispatch({ type: 'LOGIN_REQUEST' });
+        let token = null;
+        let error = '';
+        try {
+            let response = await Axios.post(API + 'api/login', {
+                email: loginDetails.username,
+                password: loginDetails.password,
+            });
+            if (response.status === 200) {
+                let data = response.data;
+                if (data.token) {
+                    token = response.data.token;
+                    dispatch({ type: 'SAVE_TOKEN', payload: token });
+                    try {
+                        let loginResponse = await Axios.get(API + 'api/login', {
+                            headers: {
+                                'auth-token': token
+                            }
+                        });
+                        if (loginResponse.status === 200) {
+                            let accountType = checkKeyInObject(checkKeyInObject(loginResponse, 'data'), 'accountType') ? loginResponse.data.accountType.toLowerCase() : 'none';
+                            if (accountType === 'vendor' || accountType === 'admin') {
+                                let userDetails = checkKeyInObject(loginResponse, 'data') ? loginResponse.data.user : {};
+                                dispatch({ type: 'LOGIN_SUCCESS', payload: { role: accountType, data: userDetails } })
+                            }
+                            else {
+                                dispatch({ type: 'LOGIN_FAILED', payload: "This web portal is only available for admin and vendors" })
+                            }
+
+                        }
+                        else {
+                            dispatch({ type: 'LOGIN_FAILED', payload: "Network Error" })
+                        }
+                    }
+                    catch (err) {
+                        dispatch({ type: 'LOGIN_FAILED', payload: "Network Error" })
+                    }
+
+                }
+                else if (data.message) {
+                    dispatch({ type: 'LOGIN_FAILED', payload: data.message })
+                }
+                else {
+                    if (checkArrayLength(data.errors)) {
+                        error = data.errors[0].msg;
+                        dispatch({ type: 'LOGIN_FAILED', payload: error })
+                    }
+                    else {
+                        dispatch({ type: 'LOGIN_FAILED', payload: "Some error occured while login" })
+                    }
+                }
+
+            }
+            else {
+                dispatch({ type: 'LOGIN_FAILED', payload: "Network Error" })
+            }
+        }
+        catch (err) {
+            console.log(err)
+            dispatch({ type: 'LOGIN_FAILED', payload: "Some error occured while login" })
+        }
     };
 };
 
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,24 +1,39 @@
 const initialState = {
-    isAuthenticated: false,
-    role: '',
+    isAuthenticated: Boolean(localStorage.getItem('vanon:token')),
+    token: localStorage.getItem('vanon:token') ? localStorage.getItem('vanon:token') : '',
+    role: localStorage.getItem('vanon:role') ? localStorage.getItem('vanon:role') : '',
     username: '',
     password: '',
     loginError: '',
+    isLoading: false,
+    user: localStorage.getItem('vanon:user') ? localStorage.getItem('vanon:user') : {},
 }
 
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'LOGIN':
-            if (action.payload.username === 'admin' && action.payload.password === '123456') {
-                return { ...state, role: 'admin', isAuthenticated: true, username: 'admin', password: '12345' }
-            }
-            else if (action.payload.username === 'vendor' && action.payload.password === '123456') {
-                return { ...state, role: 'vendor', isAuthenticated: true, username: 'vendor', password: '12345' }
-            }
-            else {
-                return { ...state, loginError: 'Invalid Username or Password' }
-            }
+        case 'LOGIN_REQUEST':
+            return { ...state, isLoading: true }
+        case 'SET_TOKEN':
+            return { ...state, token: action.payload }
+        case 'LOGIN_FAILED': {
+            localStorage.removeItem('vanon:token');
+            localStorage.removeItem('vanon:role');
+            localStorage.removeItem('vanon:user');
+            return { ...state, loginError: action.payload, isLoading: false }
+        }
+        case 'LOGIN_SUCCESS': {
+            localStorage.setItem('vanon:role', action.payload.role);
+            localStorage.setItem('vanon:user', action.payload.data);
+            return { ...state, isLoading: false, isAuthenticated: true, role: action.payload.role, user: { ...action.payload.data } }
+        }
+        case 'SAVE_TOKEN': {
+            localStorage.setItem('vanon:token', action.payload)
+            return { ...state, token: action.payload }
+        }
         case 'LOGOUT':
+            localStorage.removeItem('vanon:token');
+            localStorage.removeItem('vanon:role');
+            localStorage.removeItem('vanon:user');
             return { ...state, isAuthenticated: false }
         case 'CLEAR_AUTH_REDUCER':
             return { ...state, loginError: '' }
