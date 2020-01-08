bdbd301c91de9ac597e7d71f3e17678c8d293668
Umer Shaikh <umershaikh305@gmail.com>
2020-01-09 00:10:38 +0500

    Update Vendor Reducer

--- src/store/reducers/vendorReducer.js
+++ src/store/reducers/vendorReducer.js
@@ -1,7 +1,45 @@
 const initialState = {
     data: [
-        { name: 'Vendor1', email: 'vendor1@vanon.com', phone: '0348-9097792', cnic: '42101-452250053-5' },
-        { name: 'Vendor2', email: 'vendor2@vanon.com', phone: '0321-2570252', cnic: '42101-42225553-5' },
+        {
+            email: "badamovicz0@amazon.co.jp",
+            firstName: "Biddie",
+            lastName: "Adamovicz",
+            password: "5oCcxhh",
+            phoneNumber: "03409009795",
+            cnicId: "42311-7199985-0"
+        },
+        {
+            email: "abuckel1@go.com",
+            firstName: "Ashely",
+            lastName: "Buckel",
+            password: "1ovfL8WF",
+            phoneNumber: "03422965318",
+            cnicId: "42311-2522615-6"
+        },
+        {
+            email: "gpinnigar2@gmpg.org",
+            firstName: "Graham",
+            lastName: "Pinnigar",
+            password: "8MUMXBOj94T",
+            phoneNumber: "03495722220",
+            cnicId: "42311-4141623-4"
+        },
+        {
+            email: "hbesnardeau3@sciencedaily.com",
+            firstName: "Hewe",
+            lastName: "Besnardeau",
+            password: "PklwGjSTjL",
+            phoneNumber: "03401910876",
+            cnicId: "42311-9853920-9"
+        },
+        {
+            email: "lbail4@dot.gov",
+            firstName: "Laney",
+            lastName: "Bail",
+            password: "NG9HAzj",
+            phoneNumber: "03410873076",
+            cnicId: "42311-0124527-0"
+        }
     ]
 }
 
